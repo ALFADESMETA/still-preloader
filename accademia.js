270db1be3dc46af11e2821e27f547679b3c894c2
Alfadesmeta Services Pvt. Ltd <115678579+ALFADESMETA@users.noreply.github.com>
2024-05-15 04:55:06 +0530

    Update accademia.js

--- accademia.js
+++ accademia.js
@@ -54,4 +54,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
 // we'll still hide the overlay when the window's load event is triggered.
-window.addEventListener('load', hideOverlay);
+window.addEventListener('load');
