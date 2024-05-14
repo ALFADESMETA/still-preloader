2fc33210fb85dcc90c5787e45e32093c1d2fba39
Alfadesmeta Services Pvt. Ltd <115678579+ALFADESMETA@users.noreply.github.com>
2024-05-15 04:44:07 +0530

    Update accademia.js

--- accademia.js
+++ accademia.js
@@ -1,4 +1,3 @@
-// Create the overlay element
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
@@ -10,46 +9,41 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: black;
+    background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
 
-// Create the Lottie animation container
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    max-width: 100%;
+    max-height: 100%;
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
+lottieContainer.style.display = 'none';
 
-// Append the elements to the body
-overlay.appendChild(lottieContainer);
 document.body.appendChild(overlay);
+document.body.appendChild(lottieContainer);
 
-// Load the Lottie animation
-const animation = lottie.loadAnimation({
-    container: lottieContainer,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://alfadesmeta.github.io/still-preloader/Animation%20-%201715722968325.json' // Path to your Lottie animation JSON file
-});
-
-// Function to hide the overlay
 function hideOverlay() {
     overlay.style.display = 'none';
+    lottieContainer.style.display = 'block';
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+    lottie.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg', // ou 'canvas' se preferir
+        loop: true,
+        autoplay: true,
+        path: 'https://alfadesmeta.github.io/still-preloader/animation.json',
+    });
 }
 
-// Add an event listener to hide the overlay when all external JS files are loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
+    hideOverlay(); // Teste sem o setTimeout
 });
 
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
 window.addEventListener('load', hideOverlay);
